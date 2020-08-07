b632da8fc3877f6c762742308711b0b748cba8e7
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2020-08-07 19:58:37 -0300

    Next Level Week day 4

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+import landingImage from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+
+function Landing(){
+    const navigation = useNavigation();
+
+    function handleNavigateToStudyPages(){
+        navigation.navigate('Study');
+    }
+    function handleNavigateToGiveClassesPage(){
+        navigation.navigate('GiveClasses');
+    }
+
+    return (
+        <View style={styles.container} >
+            <Image source={landingImage} style={styles.banner}/>
+            
+            <Text style={styles.title}>
+                Seja bem-vindo, {'\n'}
+                <Text style={styles.titleBold}>
+                    O que deseja fazer?
+                </Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+                <RectButton onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon} />
+
+                    <Text>Estudar</Text>
+                </RectButton>
+
+                <RectButton onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon} />
+
+                    <Text style={styles.buttonText}>Dar Aulas</Text>
+                </RectButton>
+            </View>
+
+            <Text style={styles.totalConnections}>
+                Total de 789 conexões já realizadas {' '}
+                <Image source={heartIcon} />
+            </Text>
+        </View>
+    );
+}
+
+export default Landing;
